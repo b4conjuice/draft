9668429847a929bbf9added8b73864f8b8cdd20b
Daniel Lopez <d@niel.to>
2025-02-13 23:06:30 -0800

    feat(draft): add results

--- src/app/drafts/[id]/form.tsx
+++ src/app/drafts/[id]/form.tsx
@@ -6,18 +6,22 @@ import { type SubmitHandler, useForm } from 'react-hook-form'
 import classNames from 'classnames'
 import {
   ArrowDownOnSquareIcon,
+  ChartBarIcon,
   ChevronLeftIcon,
   Cog6ToothIcon,
   ListBulletIcon,
+  XMarkIcon,
 } from '@heroicons/react/20/solid'
 
 import { Main } from '@/components/ui'
 import { saveDraft } from '@/server/actions'
 import { type DraftNote, type DraftFields } from '@/lib/types'
+import Results from './results'
 
 export default function DraftForm(draft: DraftNote) {
   const { title, teams, items } = draft
   const [showItems, setShowItems] = useState(true)
+  const [showResults, setShowResults] = useState(false)
   const {
     register,
     handleSubmit,
@@ -47,41 +51,47 @@ export default function DraftForm(draft: DraftNote) {
   return (
     <form className='flex flex-grow flex-col' onSubmit={handleSubmit(onSubmit)}>
       <Main className='flex flex-col'>
-        <div
-          className={classNames(
-            'flex flex-grow flex-col',
-            showItems ? '' : 'hidden'
-          )}
-        >
-          <h2 className='px-2'>{title}</h2>
-          <textarea
-            className='w-full flex-grow border-cobalt bg-cobalt caret-cb-yellow focus:border-cb-mint focus:ring-0'
-            placeholder='title'
-            {...register('items', { required: true })}
-          />
-        </div>
-        <div
-          className={classNames(
-            'flex flex-grow flex-col',
-            showItems ? 'hidden' : ''
-          )}
-        >
-          <label className='px-2'>title</label>
-          <input
-            type='text'
-            className='w-full border-cobalt bg-cobalt focus:border-cb-mint focus:ring-0'
-            placeholder='title'
-            {...register('title', { required: true })}
-          />
-          <label className='px-2'>teams</label>
-          <textarea
-            className='w-full flex-grow border-cobalt bg-cobalt caret-cb-yellow focus:border-cb-mint focus:ring-0'
-            placeholder='teams'
-            {...register('teams')}
-          />
-        </div>
+        {showResults ? (
+          <Results items={items} teams={teams} />
+        ) : (
+          <>
+            <div
+              className={classNames(
+                'flex flex-grow flex-col',
+                showItems ? '' : 'hidden'
+              )}
+            >
+              <h2 className='px-2'>{title}</h2>
+              <textarea
+                className='w-full flex-grow border-cobalt bg-cobalt caret-cb-yellow focus:border-cb-mint focus:ring-0'
+                placeholder='title'
+                {...register('items', { required: true })}
+              />
+            </div>
+            <div
+              className={classNames(
+                'flex flex-grow flex-col',
+                showItems ? 'hidden' : ''
+              )}
+            >
+              <label className='px-2'>title</label>
+              <input
+                type='text'
+                className='w-full border-cobalt bg-cobalt focus:border-cb-mint focus:ring-0'
+                placeholder='title'
+                {...register('title', { required: true })}
+              />
+              <label className='px-2'>teams</label>
+              <textarea
+                className='w-full flex-grow border-cobalt bg-cobalt caret-cb-yellow focus:border-cb-mint focus:ring-0'
+                placeholder='teams'
+                {...register('teams')}
+              />
+            </div>
+          </>
+        )}
       </Main>
-      <footer className='flex items-center justify-between bg-cb-dark-blue px-2 pb-4 pt-2'>
+      <footer className='sticky bottom-0 flex items-center justify-between bg-cb-dark-blue px-2 pb-4 pt-2'>
         <div className='flex space-x-4'>
           <Link
             href='/drafts'
@@ -91,6 +101,19 @@ export default function DraftForm(draft: DraftNote) {
           </Link>
         </div>
         <div className='flex space-x-4'>
+          <button
+            className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
+            type='button'
+            onClick={() => {
+              setShowResults(!showResults)
+            }}
+          >
+            {showResults ? (
+              <XMarkIcon className='h-6 w-6' />
+            ) : (
+              <ChartBarIcon className='h-6 w-6' />
+            )}
+          </button>
           <button
             className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
             type='button'
@@ -105,7 +128,7 @@ export default function DraftForm(draft: DraftNote) {
             )}
           </button>
           <button
-            className='flex w-full justify-center text-cb-yellow hover:text-cb-yellow disabled:pointer-events-none disabled:opacity-25'
+            className='flex w-full justify-center text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
             type='submit'
             disabled={!isDirty}
           >
---
+++ src/app/drafts/[id]/results.tsx
@@ -0,0 +1,86 @@
+import copyToClipboard from '@/lib/copyToClipboard'
+import { DocumentDuplicateIcon } from '@heroicons/react/20/solid'
+
+export default function Results({
+  items,
+  teams,
+}: {
+  items: string[]
+  teams: string[]
+}) {
+  const teamsCount = teams.length
+  const league = items.reduce<string[][]>((finalItems, item, index) => {
+    const round = Math.ceil((index + 1) / teamsCount)
+    const teamIndex =
+      round % 2 === 1
+        ? index % teamsCount
+        : teamsCount - 1 - (index % teamsCount)
+    if (finalItems[teamIndex]) {
+      finalItems[teamIndex]?.push(
+        `${index + 1} ${item} (${round}-${(index % teamsCount) + 1})`
+      )
+    } else {
+      finalItems[teamIndex] = [
+        `${index + 1} ${item} (${round}-${(index % teamsCount) + 1})`,
+      ]
+    }
+    return finalItems
+  }, [])
+  return (
+    <>
+      <h2 className='px-2'>items</h2>
+      <div className='relative'>
+        <div className='absolute right-2 top-2'>
+          <button
+            className='flex w-full justify-center text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
+            type='submit'
+            onClick={() =>
+              copyToClipboard(
+                items.map((item, index) => `${index + 1} ${item}`).join('\n')
+              )
+            }
+          >
+            <DocumentDuplicateIcon className='h-6 w-6' />
+          </button>
+        </div>
+        <ol className='list-decimal bg-cobalt p-2 pl-8'>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      </div>
+      <h2 className='px-2'>teams</h2>
+      <div className='relative'>
+        <div className='absolute right-2 top-2'>
+          <button
+            className='flex w-full justify-center text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
+            type='submit'
+            onClick={() => {
+              copyToClipboard(
+                league
+                  .map(
+                    (team, index) => `${teams[index] ?? ''}\n${team.join('\n')}`
+                  )
+                  .join('\n')
+              )
+            }}
+          >
+            <DocumentDuplicateIcon className='h-6 w-6' />
+          </button>
+        </div>
+        <ul className='grid grid-cols-2 gap-4 bg-cobalt p-2 md:grid-cols-3'>
+          {league.map((team, index) => (
+            <li key={index}>
+              <h2>{teams[index]}</h2>
+              <ul>
+                {team.map((item, itemIndex) => (
+                  <li key={itemIndex}>{item}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
