e2eee7b9a141cd3377b4d63111bf7353eb42e2df
Daniel Lopez <d@niel.to>
2025-02-17 10:27:06 -0800

    feat(draft-list): add toggle to filter notes, update list item link

--- src/app/drafts/page.tsx
+++ src/app/drafts/page.tsx
@@ -8,14 +8,22 @@ import { Main } from '@/components/ui'
 import TopNav from '../_components/topNav'
 import { getNotes, saveRelatedDraft } from '@/server/actions'
 
-export default async function DraftListPage() {
+export default async function DraftListPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
   const user = await auth()
 
   if (!user.userId) {
     redirect('/')
   }
 
-  const notes = await getNotes()
+  const filter = (await searchParams).filter
+  const showAll = filter === 'all'
+
+  const allNotes = await getNotes()
+  const notes = showAll ? allNotes : allNotes.filter(note => note.hasDraft)
   return (
     <>
       <TopNav />
@@ -24,24 +32,30 @@ export default async function DraftListPage() {
           <ul className='divide-y divide-cb-dusty-blue'>
             {notes.map(note => (
               <li key={note.id} className='flex py-4 first:pt-0 last:pb-0'>
-                <span className='flex grow items-center justify-between'>
-                  <div>
-                    <div>{note.title}</div>
-                    {note.tags && note.tags.length > 0 && (
-                      <div>{note.tags.join(' ')}</div>
-                    )}
-                  </div>
-                </span>
+                {note.hasDraft ? (
+                  <Link
+                    className='text-cb-pink hover:text-cb-pink/75'
+                    href={`/drafts/${note.id}`}
+                  >
+                    <div>
+                      <div>{note.title}</div>
+                      {note.tags && note.tags.length > 0 && (
+                        <div>{note.tags.join(' ')}</div>
+                      )}
+                    </div>
+                  </Link>
+                ) : (
+                  <span className='flex grow items-center justify-between'>
+                    <div>
+                      <div>{note.title}</div>
+                      {note.tags && note.tags.length > 0 && (
+                        <div>{note.tags.join(' ')}</div>
+                      )}
+                    </div>
+                  </span>
+                )}
                 <div className='flex items-center space-x-2'>
-                  {note.hasPodcast ? (
-                    <Link
-                      className='text-cb-yellow hover:text-cb-yellow/75'
-                      href={`/drafts/${note.id}`}
-                    >
-                      {' '}
-                      <PencilSquareIcon className='h-6 w-6' />
-                    </Link>
-                  ) : (
+                  {note.hasDraft ? null : (
                     <form
                       action={async () => {
                         'use server'
@@ -56,7 +70,7 @@ export default async function DraftListPage() {
                       <button
                         type='submit'
                         tabIndex={-1}
-                        className='text-cb-orange hover:text-cb-orange/75'
+                        className='text-cb-yellow hover:text-cb-yellow/75'
                       >
                         <PencilSquareIcon className='h-6 w-6' />
                       </button>
@@ -69,7 +83,23 @@ export default async function DraftListPage() {
         </div>
       </Main>
       <footer className='sticky bottom-0 flex items-center justify-between bg-cb-dusty-blue px-2 pb-4 pt-2'>
-        <div className='flex space-x-4'></div>
+        <div className='flex space-x-4'>
+          {showAll ? (
+            <Link
+              className='text-cb-yellow hover:text-cb-yellow/75'
+              href='/drafts'
+            >
+              show only with drafts
+            </Link>
+          ) : (
+            <Link
+              className='text-cb-yellow hover:text-cb-yellow/75'
+              href='/drafts?filter=all'
+            >
+              show all notes
+            </Link>
+          )}
+        </div>
         <div className='flex space-x-4'>
           <Link
             className='text-cb-yellow hover:text-cb-yellow/75 disabled:pointer-events-none disabled:opacity-25'
